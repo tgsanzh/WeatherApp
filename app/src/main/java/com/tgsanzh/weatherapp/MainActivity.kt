c10454973b13e02d1aee77e6c7eb8d79092bb773
Sanzhar Tursynbay <tgsanzh@gmail.com>
2026-04-09 15:41:08 +0500

    Initial

---
+++ app/src/main/java/com/tgsanzh/weatherapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.tgsanzh.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tgsanzh.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
